932edfaf6e006aa5ec8d1d5dded7dba62f1c1214
DimaDK <dk1434547@gmail.com>
2025-05-16 16:33:41 +0300

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import "./Header.css";
 
 export const Header = () => {
   const { isAuthenticated, user } = useAuth();
 
   return (
-    <header className="app-header">
-      <div className="logo">
-        <Link to="/">MyBlog</Link>
-      </div>
-
-      <nav className="navigation">
+    <header className="header">
+      {/* <div className="container"> */}
+      <Link to="/" className="logo">
+        Realworld Blog
+      </Link>
+      <nav>
         {isAuthenticated ? (
           <div className="user-menu">
-            <span className="username">{user.username}</span>
-            <Link to="/new-post">Создать пост</Link>
+            <Link to="/new-article" className="create-btn">
+              Create Article
+            </Link>
+            <div className="user-info">
+              <span>{user.username}</span>
+              <img src={user.image || "/default-avatar.png"} alt="Avatar" />
+            </div>
           </div>
         ) : (
           <div className="auth-links">
-            <Link to="/login">Войти</Link>
-            <Link to="/register">Регистрация</Link>
+            <Link to="/login">
+              <button className="signIn">Sign In</button>
+            </Link>
+            <Link to="/register">
+              <button className="signUp">Sign Up</button>
+            </Link>
           </div>
         )}
       </nav>
+      {/* </div> */}
     </header>
   );
 };
